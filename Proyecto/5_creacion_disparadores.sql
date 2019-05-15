8376c53ee434f65a5840b621b48bd111a0fb2ef6
Adrián <Adravila@users.noreply.github.com>
2019-05-15 21:50:29 +0200

    Update 5_creacion_disparadores.sql

--- Proyecto/5_creacion_disparadores.sql
+++ Proyecto/5_creacion_disparadores.sql
@@ -1,7 +1,7 @@
 -- Creación de una auditoría para tener el control del sistema
 CREATE TABLE auditoria_inventario (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    DNIe INT NOT NULL,
+    DNIe VARCHAR(9)  NOT NULL,
     nombre VARCHAR(50) NOT NULL,
     apellidos VARCHAR(50) NOT NULL,
     fecha DATETIME DEFAULT NULL,
@@ -10,18 +10,21 @@ CREATE TABLE auditoria_inventario (
 );
 
 DELIMITER $$
-
 CREATE TRIGGER before_inventary_update 
-    BEFORE UPDATE ON empleado
+    BEFORE UPDATE ON inventario
     FOR EACH ROW 
 BEGIN
-	DECLARE vUser varchar(50);
+DECLARE vUser varchar(50);
     SELECT USER() INTO vUser;
-    INSERT INTO auditoria_inventario SET accion = 'update', DNIe = OLD.DNIe, nombre = OLD.nombre, apellidos = OLD.apellidos, fecha = NOW(), usuario = vUser;
+    INSERT INTO auditoria_inventario SET accion = 'update', DNIe = OLD.DNIe, nombre = OLD.nombre, apellidos = OLD.apellidos, 
+	fecha = NOW(), usuario = vUser;
+	
 END$$
+DELIMITER ;
 
+DELIMITER $$
 CREATE TRIGGER inventary_after_insert 
-    AFTER INSERT ON empleado
+    AFTER INSERT ON inventario
     FOR EACH ROW 
 BEGIN
 	DECLARE vUser varchar(50);
