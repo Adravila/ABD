cd68520488556bee33850258e9fea7fb5bd6cba6
Adrián <Adravila@users.noreply.github.com>
2019-05-15 21:20:32 +0200

    Update 4_creacion_vistas.sql

--- Proyecto/4_creacion_vistas.sql
+++ Proyecto/4_creacion_vistas.sql
@@ -8,13 +8,13 @@ CREATE OR REPLACE VIEW datos_empleados AS
 
 SELECT * FROM datos_empleados;
 
--- Vista de los datos completos que no son empleados
--- Motivo: Llevar a cabo el objetivo de conocer con más detalle la información completa de los que no son empleados
+-- Vista de los datos completos de las personas que son empleados o no
+-- Motivo: Conocer los usuarios activos del feedback, incluye el número de comentarios 
 
-CREATE OR REPLACE VIEW datos_no_empleados AS
-  SELECT P.*, E.num_comentarios
-  FROM Persona P, No_Empleado E 
-  WHERE P.dnie = E.dnie;
+CREATE OR REPLACE VIEW personas_feedback AS
+  SELECT P.*, count(*) as comentarios
+  FROM Persona P, Feedback F 
+  WHERE P.DNIe = F.DNIe
+  GROUP BY DNIe;
 
-SELECT * FROM datos_no_empleados;
-
+SELECT * FROM personas_feedback;
