1eb97336aed19afa5e7568447ad52c777024aa4d
Ernesto <37220976+ErnestoJose23@users.noreply.github.com>
2019-05-20 18:22:30 +0200

    Update 6_creacion_funcion_proc.sql

--- Proyecto/6_creacion_funcion_proc.sql
+++ Proyecto/6_creacion_funcion_proc.sql
@@ -1,14 +1,14 @@
 -- Creación de la funcion duracion_proyecto que calcula el numero de dias trabajados en un proyecto
 
 DELIMITER //
-	CREATE FUNCTION duracion_proyecto4(id INT) returns INT
+	CREATE FUNCTION duracion_proyecto(id INT) returns INT
 	BEGIN
-    DECLARE dias INT;
-    DECLARE fecha_inicio DATE;
-    DECLARE fecha_final DATE;
+    	DECLARE dias INT;
+    	DECLARE fecha_inicio DATE;
+    	DECLARE fecha_final DATE;
 	SELECT fecha_ini INTO fecha_inicio FROM Proyecto WHERE id_proyecto = id;
-	SELECT fecha_fin INTO @fecha_fin FROM Proyecto WHERE id_proyecto = id;
-	SET dias = DATEDIFF(@fecha_fin, fecha_inicio);
+	SELECT fecha_fin INTO fecha_final FROM Proyecto WHERE id_proyecto = id;
+	SET dias = DATEDIFF(fecha_final, fecha_inicio);
 	return dias;
 	END
 //
