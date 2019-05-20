d6e7b232686cc4e7290d8ee54eb8c718b194090a
Ernesto <37220976+ErnestoJose23@users.noreply.github.com>
2019-05-20 17:51:21 +0200

    Update 4_creacion_vistas.sql

--- Proyecto/4_creacion_vistas.sql
+++ Proyecto/4_creacion_vistas.sql
@@ -18,3 +18,13 @@ CREATE OR REPLACE VIEW personas_feedback AS
   GROUP BY DNIe;
 
 SELECT * FROM personas_feedback;
+
+-- Vista de los proyectos realizados en las diferentes ciudades de España junto con la dirección de sus laboratorios.
+-- Motivo: Conocer las ciudades queconsideran Smart City
+
+CREATE OR REPLACE VIEW proyectos_laboratorio AS
+	SELECT P.nombre as Nombre_Proyecto, L.direccion, C.nombre
+	FROM Proyecto P, Laboratorio L, Ciudad C
+	WHERE L.id_laboratorio = P.id_laboratorio AND L.id_ciudad = C.id_ciudad;
+
+SELECT * FROM proyectos_laboratorio;
