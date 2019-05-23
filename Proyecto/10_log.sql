3135afe6984c35ac28d12edef40a0d609c67bf71
Ernesto <37220976+ErnestoJose23@users.noreply.github.com>
2019-05-23 16:54:52 +0200

    Create 10_log.sql

---
+++ Proyecto/10_log.sql
@@ -0,0 +1,5 @@
+-- Activamos el log
+
+set global general_log=ON
+
+-- El fichero de log se encuentra en: /var/log/mysql/mysql.log, donde podremos observar los usuarios que se conectan a nuestra BD asi como sus consultas realizadas
