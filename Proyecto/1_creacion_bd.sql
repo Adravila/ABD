b39b9c54a7691c654fbfc34356bbbde523d5ab9e
Adrián <Adravila@users.noreply.github.com>
2019-05-30 14:28:46 +0200

    Update 1_creacion_bd.sql

--- Proyecto/1_creacion_bd.sql
+++ Proyecto/1_creacion_bd.sql
@@ -1,7 +1,18 @@
 CREATE DATABASE ES_Smartcity;
 use ES_Smartcity;
 
+DROP TABLE IF EXISTS Feedback;
+DROP TABLE IF EXISTS Componente;
+DROP TABLE IF EXISTS Inventario;
+DROP TABLE IF EXISTS No_Empleado;
+DROP TABLE IF EXISTS Persona;
+DROP TABLE IF EXISTS Salario;
+DROP TABLE IF EXISTS Empleado;
+DROP TABLE IF EXISTS Tribunal;
+DROP TABLE IF EXISTS Proyecto;
+DROP TABLE IF EXISTS Laboratorio;
 DROP TABLE IF EXISTS Ciudad;
+
 CREATE TABLE Ciudad (
 	id_ciudad BIGINT AUTO_INCREMENT,
 	nombre VARCHAR(50),
@@ -9,9 +20,6 @@ CREATE TABLE Ciudad (
 	constraint pk_ciudad PRIMARY KEY (id_ciudad)
 );
 
-ALTER TABLE Ciudad AUTO_INCREMENT = 1;
-
-DROP TABLE IF EXISTS Laboratorio;
 CREATE TABLE Laboratorio (
 	id_laboratorio BIGINT AUTO_INCREMENT,
 	direccion VARCHAR(100),
@@ -21,9 +29,6 @@ CREATE TABLE Laboratorio (
 	constraint fk_laboratorio_ciudad FOREIGN KEY (id_ciudad) REFERENCES Ciudad(id_ciudad)
 );
 
-ALTER TABLE Laboratorio AUTO_INCREMENT = 1;
-
-DROP TABLE IF EXISTS Proyecto;
 CREATE TABLE Proyecto (
 	id_proyecto BIGINT AUTO_INCREMENT,
 	nombre VARCHAR(100),
@@ -35,22 +40,15 @@ CREATE TABLE Proyecto (
 	id_ciudad BIGINT,
 	id_laboratorio BIGINT,
 	constraint pk_proyecto PRIMARY KEY (id_proyecto),
-	--constraint fk_proyecto_ciudad FOREIGN KEY (id_ciudad) REFERENCES Ciudad(id_ciudad),
 	constraint fk_proyecto_laboratorio FOREIGN KEY (id_laboratorio) REFERENCES Laboratorio(id_laboratorio)	
 );
 
-ALTER TABLE Proyecto AUTO_INCREMENT = 1;
-
-DROP TABLE IF EXISTS Inventario;
 CREATE TABLE Inventario (
 	id_proyecto BIGINT,
 	constraint pk_inventario PRIMARY KEY (id_proyecto),
 	constraint fk_inventario_proyecto FOREIGN KEY (id_proyecto) REFERENCES Proyecto(id_proyecto)
 );
 
-ALTER TABLE Inventario AUTO_INCREMENT = 1;
-
-DROP TABLE IF EXISTS Componente;
 CREATE TABLE Componente (
 	id_componente BIGINT AUTO_INCREMENT,
 	nombre VARCHAR(100),
@@ -63,12 +61,9 @@ CREATE TABLE Componente (
 	constraint fk_componente_inventario FOREIGN KEY (id_proyecto) REFERENCES Inventario(id_proyecto)
 );
 
-ALTER TABLE Componente AUTO_INCREMENT = 1;
-
-DROP TABLE IF EXISTS Tribunal;
 CREATE TABLE Tribunal (
 	id_tribunal BIGINT AUTO_INCREMENT,
-	fecha_aprobacion DATE,
+	fecha_publicacion DATE,
 	id_ciudad BIGINT,
 	id_proyecto BIGINT,
 	decision ENUM('Aceptado','Denegado','En espera'), 
@@ -77,9 +72,6 @@ CREATE TABLE Tribunal (
 	constraint fk_tribunal_proyecto FOREIGN KEY (id_proyecto) REFERENCES Proyecto(id_proyecto)
 );
 
-ALTER TABLE Tribunal AUTO_INCREMENT = 1;
-
-DROP TABLE IF EXISTS Persona;
 CREATE TABLE Persona (
 	DNIe VARCHAR(9),
 	nombre VARCHAR(50),
@@ -90,9 +82,6 @@ CREATE TABLE Persona (
 	constraint pk_DNIe PRIMARY KEY (DNIe)
 );
 
-ALTER TABLE Persona AUTO_INCREMENT = 1;
-
-DROP TABLE IF EXISTS Empleado;
 CREATE TABLE Empleado (
 	DNIe VARCHAR(9) REFERENCES Persona,
 	num_ss INT(11) UNIQUE,
@@ -104,12 +93,9 @@ CREATE TABLE Empleado (
 	constraint fk_empleado_proyecto FOREIGN KEY (id_proyecto) REFERENCES Proyecto(id_proyecto)
 );
 
-ALTER TABLE Empleado AUTO_INCREMENT = 1;
-
-DROP TABLE IF EXISTS Salario;
 CREATE TABLE Salario (
 	id_salario BIGINT AUTO_INCREMENT,
-	num_ss VARCHAR(20),
+	num_ss INT(11),
 	salario DOUBLE,
 	fecha_inicio DATE,
 	fecha_fin DATE,
@@ -117,17 +103,11 @@ CREATE TABLE Salario (
 	constraint fk_sueldo_empleado FOREIGN KEY (num_ss) REFERENCES Empleado(num_ss)
 );
 
-ALTER TABLE Salario AUTO_INCREMENT = 1;
-
-DROP TABLE IF EXISTS No_Empleado;
 CREATE TABLE No_Empleado (
 	DNIe VARCHAR(9) REFERENCES Persona,
 	constraint pk_no_empleado PRIMARY KEY (DNIe)
 );
 
-ALTER TABLE No_Empleado AUTO_INCREMENT = 1;
-
-DROP TABLE IF EXISTS Feedback;
 CREATE TABLE Feedback (
 	id_feedback BIGINT AUTO_INCREMENT,
 	comentario VARCHAR(500),
@@ -139,5 +119,14 @@ CREATE TABLE Feedback (
 	constraint fk_feedback_persona FOREIGN KEY (DNIe) REFERENCES Persona(DNIe)	
 );
 
+ALTER TABLE Tribunal AUTO_INCREMENT = 1;
 ALTER TABLE Feedback AUTO_INCREMENT = 1;
-
+ALTER TABLE Inventario AUTO_INCREMENT = 1;
+ALTER TABLE No_Empleado AUTO_INCREMENT = 1;
+ALTER TABLE Salario AUTO_INCREMENT = 1;
+ALTER TABLE Persona AUTO_INCREMENT = 1;
+ALTER TABLE Laboratorio AUTO_INCREMENT = 1;
+ALTER TABLE Ciudad AUTO_INCREMENT = 1;
+ALTER TABLE Proyecto AUTO_INCREMENT = 1;
+ALTER TABLE Componente AUTO_INCREMENT = 1;
+ALTER TABLE Empleado AUTO_INCREMENT = 1;
